0aefc302297aaa3a1cac1d4e18ae27d53c3dfff3
Michelle Beshears <meeshysu@gmail.com>
2018-10-22 18:12:40 -0500

    all finished

--- javascripts/events.js
+++ javascripts/events.js
@@ -11,7 +11,22 @@ const filterButtons = () => {
    $('#morning').click(() => {
        $('.locationsOf').show();
        $('.locationsOf').not('.Morning').hide();
-   })
+   });
+   $('#afternoon').click(() => {
+       $('.locationsOf').show();
+       $('.locationsOf').not('.Afternoon').hide();
+   });
+   $('#evening').click(() => {
+    $('.locationsOf').show();
+    $('.locationsOf').not('.Evening').hide();
+   });
+   $('#afterDark').click(() => {
+    $('.locationsOf').show();
+    $('.locationsOf').not('.After').hide();
+   });
+   $('#every').click(() => {
+    $('.locationsOf').show();
+});
 }
 
 export { filterButtons, filterSearch }
